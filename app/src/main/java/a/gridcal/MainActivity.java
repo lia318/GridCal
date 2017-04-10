a7f025bd96bea2c122483e53b1d0bc1e42e4add1
lia318 <lia318@e-mirim.hs.kr>
2017-04-10 20:42:23 +0900

    #2 Event Programming

--- app/src/main/java/a/gridcal/MainActivity.java
+++ app/src/main/java/a/gridcal/MainActivity.java
@@ -1,7 +1,9 @@
 package a.gridcal;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,8 +11,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit_1, edit_2;
-    Button[] butNums=new Button[10];
-    Button[] butOps=new Button[4]
+    Button[] butNums = new Button[10];
+    Button[] butOps = new Button[4];
+    public static final int SELECT_EDIT1=0;
+    public static final int SELECT_EDIT2=1;
+    int SelectEdit = SELECT_EDIT1;
+    String numStr = "";
     // int[] ids = {R.id.but_0..... but_9까지 값을 초기화 해준다. => 미개한 방법} => for문 ids[i]
     Button but_plus, but_minus, but_multiply, but_division;
     TextView textResult;
@@ -21,50 +27,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        for(int i=0; i<butNums.length; i++){
-            butNums[i]=(Button)findViewById(R.id.but_0 + i);
-            butNums[i].setOnClickListener();
-        }
-        for(int i=0; i<butOps.length; i++){
-            butOps[i]=(Button)findViewById(R.id.but_0 + i);
-            butOps[i].setOnClickListener();
-        }
-
         edit_1 = (EditText) findViewById(R.id.edit_1);
         edit_2 = (EditText) findViewById(R.id.edit_2);
+        edit_1.setOnTouchListener(edithandler);
+        edit_2.setOnTouchListener(edithandler);
 
-        but_plus = (Button) findViewById(R.id.but_plus);
-        but_minus = (Button) findViewById(R.id.but_minus);
-        but_multiply = (Button) findViewById(R.id.but_multiply);
-        but_division = (Button) findViewById(R.id.but_division);
-
-        textResult = (TextView) findViewById(R.id.text_result);
-
-        // setOnClickListener가 있어야만 버튼 클릭이 가능해진다.
-        but_plus.setOnClickListener(butHandler);
-        but_minus.setOnClickListener(butHandler);
-        but_multiply.setOnClickListener(butHandler);
-        but_division.setOnClickListener(butHandler);
+        for (int i = 0; i < butNums.length; i++) {
+            butNums[i] = (Button) findViewById(R.id.but_0 + i);
+            butNums[i].setOnClickListener(butNumHandler);
+        }
+        for (int i = 0; i < butOps.length; i++) {
+            butOps[i] = (Button) findViewById(R.id.but_0 + i);
+            butOps[i].setOnClickListener(butNumHandler);
+        }
     }
 
-    View.OnClickListener butNumHandler=new View.OnClickListener(){
-
+    View.OnClickListener butNumHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            Button but = (Button)v;
+            numStr +=but.getText(); // 버튼이 클릭될 때마다 누적
 
+            switch(SelectEdit){
+                case SELECT_EDIT1:
+                    edit_1.setText(numStr);
+                    break;
+                case SELECT_EDIT2:
+                    edit_2.setText(numStr);
+                    break;
+            }
         }
     };
 
-    View.OnClickListener butOpsHandler=new View.OnClickListener(){
+    View.OnClickListener butOpsHandler = new View.OnClickListener() {
 
-        @Override
-        public void onClick(View v) {
-
-        }
-    };
-
-    View.OnClickListener butHandler = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             int first_num = Integer.parseInt(edit_1.getText().toString());
@@ -84,8 +80,26 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.but_division:
                     result = first_num / second_num;
                     break;
+
             }
-            textResult.setText("계산결과 : "+result + "");
+            textResult.setText(result + "");
+        }
+    };
+
+    View.OnTouchListener edithandler = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            numStr = "";
+
+            switch(v.getId()){
+                case R.id.edit_1:
+                    SelectEdit = SELECT_EDIT1;
+                    break;
+                case R.id.edit_2:
+                    SelectEdit = SELECT_EDIT2;
+                    break;
+            }
+            return true;
         }
     };
 }
